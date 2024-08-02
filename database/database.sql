c36267012c69dea4f8877ab27104502cc56b6547
Gabriel Bianchi Britto (Bolsista) <gabriel.britto@cefetes.br>
2024-08-02 12:28:09 -0300

    first commit

--- database/database.sql
+++ database/database.sql
@@ -1,3 +1,13 @@
 CREATE DATABASE IF NOT EXISTS `filmesAndSeries`;
 USE `filmesAndSeries`;
 
+CREATE TABLE IF NOT EXISTS `filmesAndSeries`.`usuarios` (
+	idUsuario INT PRIMARY KEY,
+    nome VARCHAR (80),
+    email VARCHAR (80),
+    senha VARCHAR (80)
+);
+
+insert into usuarios values (1, 'Gabriel', 'cabecinha@guidao.com', 'senha');
+
+SELECT * FROM `filmesAndSeries`.`usuarios`;
